6e54e9cac7873390deee3db1914e4e0972564608
Jan <jackflash5500@gmail.com>
2017-02-04 12:30:53 +0100

    Added support overwriting the game on each launch. The switch for that is stored in the use settings.

---
+++ love-launcher/DEVELOPMENT.md
@@ -0,0 +1,5 @@
+Steps:
+1. clone the repo
+2. run npm install to get all modules
+3. open the repo in vscode and debug with F5
+4. contribute back to this repo and let me push it to prod
--- love-launcher/src/extension.ts
+++ love-launcher/src/extension.ts
@@ -1,30 +1,44 @@
 'use strict';
 import * as vscode from 'vscode';
 var exec = require('child_process').execFile;
-
+var currentInstances = [];
 
 export function activate(context: vscode.ExtensionContext) {
 
-    var currentInstances = 0;
+    
     var maxInstances = vscode.workspace.getConfiguration('lövelauncher').get('maxInstances');
+    var overWrite = vscode.workspace.getConfiguration('lövelauncher').get('overWrite');
   
 
     let disposable = vscode.commands.registerCommand('lövelauncher.launch', () => {
 
-        if(currentInstances < maxInstances){
+        if(currentInstances.length < maxInstances || overWrite){
             var path : string = vscode.workspace.getConfiguration('lövelauncher').get('path').toString();
             var useConsoleSubsystem = vscode.workspace.getConfiguration('lövelauncher').get('useConsoleSubsystem');
+
+            if (overWrite){
+                currentInstances.forEach(function(instance){
+                    if (instance != undefined){
+                        instance.kill();
+                    }
+                });
+            }
             
-            currentInstances++;
             if(!useConsoleSubsystem){
-                exec(path, [vscode.workspace.rootPath], function(err, data) {  
-                    currentInstances--;                
-                });  
+                
+                var process = (exec(path, [vscode.workspace.rootPath], function(err, data) {  
+              
+                }));  
+                process.on('exit', on_exit.bind(null,process));
+                currentInstances[process.pid] = process;
             }else{
+                
                 path = path.substr(0,path.lastIndexOf(".")) + "c.exe";
-                exec(path, [vscode.workspace.rootPath], function(err, data) {  
-                    currentInstances--;                
+                var process = exec(path, [vscode.workspace.rootPath], function(err, data) {  
+                    //currentInstances--;                
                 });  
+                process.on('exit', on_exit.bind(null,process));
+                currentInstances[process.pid] = process;
             }
         }else{
             vscode.window.showErrorMessage("You have reached your max concurrent Löve instances. You can change this setting in your config.");
@@ -36,4 +50,9 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 export function deactivate() {
+}
+
+function on_exit(oprocess){
+    currentInstances.splice(oprocess.pid, 1);
+    currentInstances = currentInstances.filter(Boolean);
 }
